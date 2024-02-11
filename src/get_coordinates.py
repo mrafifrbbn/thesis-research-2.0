36cec3faf5d1523f1c5dde7c9507cfe0a3dedd6b
mrafifrbbn <mrafifrbbn@gmail.com>
2024-02-11 14:02:45 +0700

    chore: convert csv to IPAC format

--- src/get_coordinates.py
+++ src/get_coordinates.py
@@ -9,7 +9,6 @@ ROOT_PATH = os.environ.get('ROOT_PATH')
 SDFGS_FILEPATH = os.path.join(ROOT_PATH, 'data/raw/6dfgs/campbell_table8.ascii')
 SDSS_FILEPATH = os.path.join(ROOT_PATH, 'data/raw/sdss/SDSS_spectro_mrafifrbbn.csv')
 LAMOST_FILEPATH = os.path.join(ROOT_PATH, 'data/raw/lamost/lamost_DR7_VDcat_20200825.fits')
-OUTPUT_PATH = os.path.join(ROOT_PATH, 'data/raw/sky_coord/sky_coord.csv')
 
 def get_eq_coords():
     '''
@@ -24,24 +23,22 @@ def get_eq_coords():
     # 6dFGS galaxies: ra still in hour, so need to convert to degrees
     df1 = pd.read_csv(SDFGS_FILEPATH, delim_whitespace=True)[['ra', 'dec']]
     df1['ra'] = df1['ra']*15
-    logger.info(f'Total 6dFGS coordinates: {len(df1)}')
+    output_path_6dfgs = os.path.join(ROOT_PATH, 'data/preprocessed/sky_coord/6dfgs.csv')
+    logger.info(f'Total 6dFGS galaxies: {len(df1)}. Saving the coordinates to {output_path_6dfgs}')
+    df1.to_csv(output_path_6dfgs, index=False)
 
     # SDSS galaxies
     df2 = pd.read_csv(SDSS_FILEPATH)[['ra', 'dec']]
-    logger.info(f'Total SDSS coordinates: {len(df2)}')
+    output_path_sdss = os.path.join(ROOT_PATH, 'data/preprocessed/sky_coord/sdss.csv')
+    logger.info(f'Total SDSS galaxies: {len(df2)}. Saving the coordinates to {output_path_sdss}')
+    df2.to_csv(output_path_sdss, index=False)
 
     # LAMOST galaxies
     with fits.open(LAMOST_FILEPATH) as hdul:
         df3 = pd.DataFrame(hdul[1].data)[['ra', 'dec']]
-    logger.info(f'Total LAMOST coordinates: {len(df3)}')
-        
-    # Concatenate the three coordinates
-    df = pd.concat([df1, df2, df3])
-    logger.info(f'Total coordinate obtained: {len(df)}')
-
-    # Save the coordinates as csv file
-    logger.info(f'Saving the coordinates at {OUTPUT_PATH}')
-    df.to_csv(OUTPUT_PATH, index=False)
+    output_path_lamost = os.path.join(ROOT_PATH, 'data/preprocessed/sky_coord/lamost.csv')
+    logger.info(f'Total LAMOST galaxies: {len(df3)}. Saving the coordinates to {output_path_lamost}')
+    df3.to_csv(output_path_lamost, index=False)
 
     return
 

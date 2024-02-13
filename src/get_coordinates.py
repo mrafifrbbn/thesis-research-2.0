a25f1fc218cabdf6a4b12049e50fd8d8f0489d33
mrafifrbbn <mrafifrbbn@gmail.com>
2024-02-13 10:30:47 +0700

    chore: save coordinates directly as IPAC format

--- src/get_coordinates.py
+++ src/get_coordinates.py
@@ -1,6 +1,7 @@
 import os
 import pandas as pd
 from astropy.io import fits
+from astropy.table import Table
 from utils.logging_config import get_logger
 from dotenv import load_dotenv
 load_dotenv()
@@ -12,7 +13,7 @@ LAMOST_FILEPATH = os.path.join(ROOT_PATH, 'data/raw/lamost/lamost_DR7_VDcat_2020
 
 def get_eq_coords():
     '''
-    Combining the equatorial coordinates for all the galaxies in the raw data.
+    Combining the equatorial coordinates for all the galaxies in the raw data and save them in IPAC format.
     These coordinates are fed into the 2MASS XSC to obtain the 2MASS photometry.
     '''
 
@@ -23,22 +24,22 @@ def get_eq_coords():
     # 6dFGS galaxies: ra still in hour, so need to convert to degrees
     df1 = pd.read_csv(SDFGS_FILEPATH, delim_whitespace=True)[['ra', 'dec']]
     df1['ra'] = df1['ra']*15
-    output_path_6dfgs = os.path.join(ROOT_PATH, 'data/preprocessed/sky_coord/6dfgs.csv')
-    logger.info(f'Total 6dFGS galaxies: {len(df1)}. Saving the coordinates to {output_path_6dfgs}')
-    df1.to_csv(output_path_6dfgs, index=False)
+    output_path = os.path.join(ROOT_PATH, 'data/preprocessed/sky_coord/6dfgs.ascii')
+    logger.info(f'Total 6dFGS galaxies: {len(df1)}. Saving the coordinates to {output_path}')
+    Table.from_pandas(df1).write(output_path, format='ipac', overwrite=True)
 
     # SDSS galaxies
     df2 = pd.read_csv(SDSS_FILEPATH)[['ra', 'dec']]
-    output_path_sdss = os.path.join(ROOT_PATH, 'data/preprocessed/sky_coord/sdss.csv')
-    logger.info(f'Total SDSS galaxies: {len(df2)}. Saving the coordinates to {output_path_sdss}')
-    df2.to_csv(output_path_sdss, index=False)
+    output_path = os.path.join(ROOT_PATH, 'data/preprocessed/sky_coord/sdss.ascii')
+    logger.info(f'Total SDSS galaxies: {len(df2)}. Saving the coordinates to {output_path}')
+    Table.from_pandas(df2).write(output_path, format='ipac', overwrite=True)
 
     # LAMOST galaxies
     with fits.open(LAMOST_FILEPATH) as hdul:
         df3 = pd.DataFrame(hdul[1].data)[['ra', 'dec']]
-    output_path_lamost = os.path.join(ROOT_PATH, 'data/preprocessed/sky_coord/lamost.csv')
-    logger.info(f'Total LAMOST galaxies: {len(df3)}. Saving the coordinates to {output_path_lamost}')
-    df3.to_csv(output_path_lamost, index=False)
+    output_path = os.path.join(ROOT_PATH, 'data/preprocessed/sky_coord/lamost.ascii')
+    logger.info(f'Total LAMOST galaxies: {len(df3)}. Saving the coordinates to {output_path}')
+    Table.from_pandas(df3).write(output_path, format='ipac', overwrite=True)
 
     return
 

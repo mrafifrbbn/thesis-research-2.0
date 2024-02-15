d7a10489c9c98701eaa76d0ebefb981e488987e7
mrafifrbbn <mrafifrbbn@gmail.com>
2024-02-15 19:53:46 +0700

    add: error handler for log

--- src/utils/logging_config.py
+++ src/utils/logging_config.py
@@ -12,6 +12,7 @@ def get_logger(logger_id, current_date=datetime.datetime.now().strftime('%Y%m%d'
 
     # Log file name
     log_file = os.path.join(ROOT_PATH, f'log/{current_date}_log.txt')
+    err_file = os.path.join(ROOT_PATH, f'log/{current_date}_error.txt')
 
     # Create a logger instance 
     logger = logging.getLogger(logger_id)
@@ -21,11 +22,17 @@ def get_logger(logger_id, current_date=datetime.datetime.now().strftime('%Y%m%d'
     info_handler = logging.FileHandler(log_file)
     info_handler.setLevel(logging.INFO)
 
+    # Create file handler that logs ERROR and higher level messages
+    error_handler = logging.FileHandler(err_file)
+    error_handler.setLevel(logging.ERROR)
+
     # Create formatter and add it to the handlers
     formatter = logging.Formatter("[%(asctime)s] [%(levelname)s] : %(message)s", "%Y-%m-%d %H:%M:%S %p")
     info_handler.setFormatter(formatter)
+    error_handler.setFormatter(formatter)
 
     # Add the handlers to logger
     logger.addHandler(info_handler)
+    logger.addHandler(error_handler)
 
     return logger

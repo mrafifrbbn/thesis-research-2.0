16656015f713ad019abc8a6d98d9839702aca292
mrafifrbbn <mrafifrbbn@gmail.com>
2024-02-19 11:37:26 +0700

    chore: create path variables

--- src/get_coordinates.py
+++ src/get_coordinates.py
@@ -7,14 +7,21 @@ from utils.logging_config import get_logger
 from dotenv import load_dotenv
 load_dotenv()
 
+# Environment variable
+ROOT_PATH = os.environ.get('ROOT_PATH')
+
 # Create logging instance
 logger = get_logger('get_eq_coords')
 
-# File paths
-ROOT_PATH = os.environ.get('ROOT_PATH')
-SDFGS_FILEPATH = os.path.join(ROOT_PATH, 'data/raw/6dfgs/sdfgs_fp_vizier.fits')
-SDSS_FILEPATH = os.path.join(ROOT_PATH, 'data/raw/sdss/SDSS_spectro.csv')
-LAMOST_FILEPATH = os.path.join(ROOT_PATH, 'data/raw/lamost/lamost_DR7_VDcat_20200825.fits')
+# Input file paths
+SDFGS_INPUT_FILEPATH = os.path.join(ROOT_PATH, 'data/raw/6dfgs/sdfgs_fp_vizier.fits')
+SDSS_INPUT_FILEPATH = os.path.join(ROOT_PATH, 'data/raw/sdss/SDSS_spectro.csv')
+LAMOST_INPUT_FILEPATH = os.path.join(ROOT_PATH, 'data/raw/lamost/lamost_DR7_VDcat_20200825.fits')
+
+# Output file paths
+SDFGS_OUTPUT_FILEPATH = os.path.join(ROOT_PATH, 'data/preprocessed/sky_coord/6dfgs.ascii')
+SDSS_OUTPUT_FILEPATH = os.path.join(ROOT_PATH, 'data/preprocessed/sky_coord/sdss.ascii')
+LAMOST_OUTPUT_FILEPATH = os.path.join(ROOT_PATH, 'data/preprocessed/sky_coord/lamost.ascii')
 
 def get_eq_coords():
     '''
@@ -23,26 +30,23 @@ def get_eq_coords():
     '''
     try:
         # 6dFGS galaxies: ra still in hour, so need to convert to degrees
-        with fits.open(SDFGS_FILEPATH) as hdul:
+        with fits.open(SDFGS_INPUT_FILEPATH) as hdul:
             df1 = Table(hdul[1].data).to_pandas()[['RAJ2000', 'DEJ2000']]
         df1['RAJ2000'] *= 15
         df1 = df1.rename({'RAJ2000': 'ra', 'DEJ2000': 'dec'}, axis=1)
-        output_path = os.path.join(ROOT_PATH, 'data/preprocessed/sky_coord/6dfgs.ascii')
-        logger.info(f'Total 6dFGS galaxies: {len(df1)}. Saving the coordinates to {output_path}')
-        Table.from_pandas(df1).write(output_path, format='ipac', overwrite=True)
+        logger.info(f'Total 6dFGS galaxies: {len(df1)}. Saving the coordinates to {SDFGS_OUTPUT_FILEPATH}')
+        Table.from_pandas(df1).write(SDFGS_OUTPUT_FILEPATH, format='ipac', overwrite=True)
 
         # SDSS galaxies
-        df2 = pd.read_csv(SDSS_FILEPATH)[['ra', 'dec']]
-        output_path = os.path.join(ROOT_PATH, 'data/preprocessed/sky_coord/sdss.ascii')
-        logger.info(f'Total SDSS galaxies: {len(df2)}. Saving the coordinates to {output_path}')
-        Table.from_pandas(df2).write(output_path, format='ipac', overwrite=True)
+        df2 = pd.read_csv(SDSS_INPUT_FILEPATH)[['ra', 'dec']]
+        logger.info(f'Total SDSS galaxies: {len(df2)}. Saving the coordinates to {SDSS_OUTPUT_FILEPATH}')
+        Table.from_pandas(df2).write(SDSS_OUTPUT_FILEPATH, format='ipac', overwrite=True)
 
         # LAMOST galaxies
-        with fits.open(LAMOST_FILEPATH) as hdul:
+        with fits.open(LAMOST_INPUT_FILEPATH) as hdul:
             df3 = pd.DataFrame(hdul[1].data)[['ra', 'dec']]
-        output_path = os.path.join(ROOT_PATH, 'data/preprocessed/sky_coord/lamost.ascii')
-        logger.info(f'Total LAMOST galaxies: {len(df3)}. Saving the coordinates to {output_path}')
-        Table.from_pandas(df3).write(output_path, format='ipac', overwrite=True)
+        logger.info(f'Total LAMOST galaxies: {len(df3)}. Saving the coordinates to {LAMOST_OUTPUT_FILEPATH}')
+        Table.from_pandas(df3).write(LAMOST_OUTPUT_FILEPATH, format='ipac', overwrite=True)
     
     except Exception as e:
         logger.error(f'Fetching coordinates failed. Reason: {e}')

cfa6e1bb80fbfa482f5f37cf98a9643a41bd564e
mrafifrbbn <mrafifrbbn@gmail.com>
2025-06-10 11:31:51 +0700

    MAIN: refactor main_code structure

---
+++ main_code/section_3_fp/step_3.3_fit_mocks.py
@@ -0,0 +1,239 @@
+import os
+import sys
+import re
+import time
+import numpy as np
+import pandas as pd
+import scipy as sp
+import argparse
+from pathlib import Path
+
+from utils.constants import *
+from utils.CosmoFunc import *
+from utils.logging_config import get_logger
+
+from dotenv import load_dotenv
+load_dotenv(override=True)
+
+# Create logging instance
+logger = get_logger('fit_mocks')
+
+ROOT_PATH = os.environ.get("ROOT_PATH")
+if not ROOT_PATH in sys.path: sys.path.append(ROOT_PATH)
+
+from main_code.utils.filepaths import *
+from main_code.utils.constants import *
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--id-start", help="Mock ID to start fitting over.", type=int, default=1
+)
+parser.add_argument(
+    "--id-end", help="Mock ID to end fitting over.", type=int, default=1000
+)
+parser.add_argument(
+    "--fresh-start", help="Deletes the output file (if exists) if called.", action="store_true"
+)
+
+# Start and end ID of the mock (change if something fails midway)
+args = parser.parse_args()
+ID_START = args.id_start
+ID_END = args.id_end
+
+def fit_mock(
+        mock_filename: str,
+        survey: str,
+        fp_fit_method: int,
+        smin_setting: int,
+        id_start: int,
+        id_end: int,
+        output_filepath: str,
+        reject_outliers: bool = False,
+        pvals_cut: float = PVALS_CUT
+        ):
+    """_summary_
+
+    Args:
+        mock_filename (str): _description_
+        survey (str): _description_
+        fp_fit_method (int): _description_
+        smin_setting (int): _description_
+        id_start (int): _description_
+        id_end (int): _description_
+        output_filepath (str): _description_
+    """
+    
+    logger.info(f'Fitting {id_end - id_start + 1} mocks of {survey} | FP Fit Method = {fp_fit_method} | SMIN_SETTING = {smin_setting}')
+
+    # Create boolean to store whether to use full f_n or partial f_n
+    use_full_fn = True if fp_fit_method == 0 else False
+
+    # Set veldisp lower limit
+    smin = SURVEY_VELDISP_LIMIT[smin_setting][survey]
+
+    # Load the mock galaxies data
+    mock = pd.read_csv(mock_filename, delim_whitespace=True)
+
+    start = time.time()
+    for mock_id in range(id_start, id_end + 1):
+        df = mock.copy()
+
+        # Select one mock realization at a time to fit
+        df = df[df['#mockgal_ID'].apply(lambda x: int(x.split('_')[-1])) == mock_id]
+
+        # Calculate redshift
+        df['z'] = df['cz'] / LIGHTSPEED
+
+        # Calculate predicted true distance and FN integral limits
+        red_spline, lumred_spline, dist_spline, lumdist_spline, ez_spline = rz_table()
+        d_H = sp.interpolate.splev(df['z'].to_numpy(), dist_spline, der=0)
+        df['lmin'] = (SOLAR_MAGNITUDE['j'] + 5.0 * np.log10(1.0 + df["z"].to_numpy()) + df["kcorr"].to_numpy() + df["extinction_j"].to_numpy() + 10.0 - 2.5 * np.log10(2.0 * math.pi) + 5.0 * np.log10(d_H) - MAG_HIGH) / 5.0
+        df['lmax'] = (SOLAR_MAGNITUDE['j'] + 5.0 * np.log10(1.0 + df["z"].to_numpy()) + df["kcorr"].to_numpy() + df["extinction_j"].to_numpy() + 10.0 - 2.5 * np.log10(2.0 * math.pi) + 5.0 * np.log10(d_H) - MAG_LOW) / 5.0
+
+        # Assume 0 peculiar velocities
+        df['logdist_pred'] = 0.0
+        df['r_true'] = df['r'] - df['logdist_pred']
+
+        if not use_full_fn:
+            # logger.info(f"Original # of galaxies: {len(df)}")
+            # df = df[df["Sprob"] >= 0.005]
+            Sn = df["Sprob"].to_numpy()
+            # logger.info(f"Remaining # of galaxies after Sprob cut: {len(df)}")
+        else:
+            Sn = 1.0
+
+        df['Sn'] = Sn
+        df['C_m'] = 1.0
+
+        # Range of FP parameters
+        if use_full_fn:
+            param_boundaries = PARAM_BOUNDARIES
+        else:
+            param_boundaries = [(1.2, 1.8), (-1.1, -0.7), (-0.2, 0.4), (2.1, 2.4), (3.1, 3.5), (0.0, 0.06), (0.20, 0.45), (0.1, 0.25)]
+
+        avals, bvals = param_boundaries[0], param_boundaries[1]
+        rvals, svals, ivals = param_boundaries[2], param_boundaries[3], param_boundaries[4]
+        s1vals, s2vals, s3vals = param_boundaries[5], param_boundaries[6], param_boundaries[7]
+
+        # Fitting the FP iteratively
+        data_fit = df
+        badcount = len(df)
+        is_converged = False
+        i = 1
+        while not is_converged:
+
+            Snfit = data_fit["Sn"].to_numpy()
+
+            # Fit the FP parameters
+            FPparams = sp.optimize.differential_evolution(FP_func, bounds=(avals, bvals, rvals, svals, ivals, s1vals, s2vals, s3vals), 
+                args=(0.0, data_fit["z"].to_numpy(), data_fit["r_true"].to_numpy(), data_fit["s"].to_numpy(), data_fit["i"].to_numpy(), data_fit["dr"].to_numpy(), data_fit["ds"].to_numpy(), data_fit["di"].to_numpy(), Snfit, smin, data_fit["lmin"].to_numpy(), data_fit["lmax"].to_numpy(), data_fit["C_m"].to_numpy(), True, False, use_full_fn), maxiter=10000, tol=1.0e-6, workers=-1)
+
+            # Break from the loop if not iterative
+            if reject_outliers == False:
+                break
+
+            # Calculate the chi-squared 
+            chi_squared = Sn * FP_func(FPparams.x, 0.0, df["z"].to_numpy(), df["r_true"].to_numpy(), df["s"].to_numpy(), df["i"].to_numpy(), df["dr"].to_numpy(), df["ds"].to_numpy(), df["di"].to_numpy(), Sn, smin, df["lmin"].to_numpy(), df["lmax"].to_numpy(), df["C_m"].to_numpy(), sumgals=False, chi_squared_only=True, use_full_fn=use_full_fn)[0]
+            
+            # Calculate the p-value (x,dof)
+            pvals = sp.stats.chi2.sf(chi_squared, np.sum(chi_squared)/(len(df) - 8.0))
+            
+            # Reject galaxies with p-values < pvals_cut (probabilities of being part of the sample lower than some threshold)
+            data_fit = df.drop(df[pvals < pvals_cut].index).reset_index(drop=True)
+            
+            # Count the number of rejected galaxies
+            badcountnew = len(np.where(pvals < pvals_cut)[0])
+            
+            # Converged if the number of rejected galaxies in this iteration is the same as previous iteration
+            is_converged = True if badcount == badcountnew else False
+            
+            # Set the new count of rejected galaxies
+            badcount = badcountnew
+            i += 1
+
+        # logger.info verbose
+        print(f"{'-' * 10} Mock {mock_id} {'-' * 10} | FP parameters: {FPparams.x.tolist()}")
+
+        df = data_fit
+        
+        # Append the results to the output file
+        with open(output_filepath, "a") as myfile:
+            text = ','.join([str(mock_id)] + [str(x) for x in FPparams.x]) + '\n'
+            myfile.write(text)
+
+    logger.info(f'Fitting mocks successful! Time elapsed = {round(time.time() - start, 2)} s.')
+    return
+
+
+def main():
+
+    mock_filenames = os.listdir(MOCK_DATA_FILEPATH)
+
+    for filename in mock_filenames:
+        if not filename.endswith(".txt"):
+            continue
+        
+        logger.info(f"Reading file: {filename}")
+
+        # Parse the filename to retrieve settings
+        survey = re.search(r"(.*)_mocks", filename).group(1)
+
+        if survey.lower() == '6dfgs':
+            survey = '6dFGS'
+        else:
+            survey = survey.upper()
+
+        smin_setting = int(re.search(r"smin_(\d+)", filename).group(1))
+        fp_fit_method_filename = int(re.search(r"fp_fit_method_(\d+)", filename).group(1))
+        
+        # For now, focus on mocks generated from full_fn FP
+        if fp_fit_method_filename != 0:
+            continue
+
+        # AD HOC: do for LAMOST only
+        if survey != "LAMOST":
+            continue
+
+        # Fit the mocks with the two methods
+        for fp_fit_method in [1]:
+            # Generate output file path string
+            method_ = "full" if fp_fit_method == 0 else "partial"
+            output_filepath = Path(os.path.join(ROOT_PATH, f"artifacts/mock_fits/smin_setting_{smin_setting}/fp_fit_method_{fp_fit_method_filename}/{survey.lower()}_fit_with_{method_}_fn_no_Sn_cut.csv"))
+
+            # Create parent directory for the mock fits
+            if not os.path.exists(output_filepath.parent):
+                os.makedirs(output_filepath.parent)
+
+            # Deletes file if fresh_start is called
+            if args.fresh_start:
+                if os.path.exists(output_filepath):
+                    os.remove(output_filepath)
+                with open(output_filepath, "w") as fp:
+                    fp.write("mock_id,a,b,rmean,smean,imean,sigma1,sigma2,sigma3\n")
+                last_id = 1
+            else:
+                # Create file if not exist
+                if not os.path.exists(output_filepath):
+                    with open(output_filepath, "w") as fp:
+                        fp.write("mock_id,a,b,rmean,smean,imean,sigma1,sigma2,sigma3\n")
+                        last_id = 1
+                # Else, fetch the latest mock ID
+                else:
+                    last_id = int(pd.read_csv(output_filepath)['mock_id'].max()) + 1
+
+            logger.info(f"Fitting mock: {filename} | survey: {survey} | fp_fit_method: {fp_fit_method} | output_filepath: {output_filepath}")
+            fit_mock(
+                mock_filename=os.path.join(MOCK_DATA_FILEPATH, filename),
+                survey=survey,
+                fp_fit_method=fp_fit_method,
+                smin_setting=smin_setting,
+                id_start=last_id,
+                id_end=ID_END,
+                output_filepath=output_filepath,
+                reject_outliers=False
+            )
+
+
+if __name__ == '__main__':
+    main()
---
+++ main_code/section_4_logdist/step_4.1_fit_logdist.py
@@ -0,0 +1,310 @@
+import os
+import sys
+import time
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import scipy as sp
+from scipy.stats import norm
+from scipy.optimize import curve_fit
+from scipy.special import erf
+
+from dotenv import load_dotenv
+load_dotenv(override=True)
+
+ROOT_PATH = os.environ.get('ROOT_PATH')
+if not ROOT_PATH in sys.path: sys.path.append(ROOT_PATH)
+
+from main_code.utils.constants import *
+from main_code.utils.CosmoFunc import *
+from main_code.utils.functions import gaus, skewnormal
+from main_code.utils.filepaths import (
+    OUTLIER_REJECT_FP_SAMPLE_FILEPATHS,
+    FP_FIT_FILEPATH,
+    FP_FIT_ABC_FIXED_FILEPATH,
+    LOGDIST_POSTERIOR_OUTPUT_FILEPATH,
+    LOGDIST_OUTPUT_FILEPATH,
+    CURVEFIT_COMPARISON_IMG_FILEPATH,
+    POSTERIOR_SKEWNESS_IMG_FILEPATH
+)
+from main_code.utils.logging_config import get_logger
+
+pvhub_dir = os.environ.get('PVHUB_DIR_PATH')
+if not pvhub_dir in sys.path: sys.path.append(pvhub_dir)
+from pvhub import * # type: ignore
+
+SMIN_SETTING = int(os.environ.get('SMIN_SETTING'))
+FP_FIT_METHOD = int(os.environ.get('FP_FIT_METHOD'))
+
+# Create boolean from FP_FIT_METHOD value
+USE_FULL_FN = True if FP_FIT_METHOD == 0 else False
+
+# Create logging instance
+logger = get_logger('fit_logdist')
+
+
+def fit_logdist(
+        survey: str,
+        df: pd.DataFrame,
+        smin: float,
+        FPmethod: str,
+        FPparams: np.ndarray,
+        use_full_fn: bool = True,
+        mag_high: float = MAG_HIGH,
+        mag_low: float = MAG_LOW,
+        save_posterior: bool = False,
+        with_pv_model: bool = False
+    ) -> pd.DataFrame:
+    """
+    This is a function to calculate the log-distance ratio posteriors and obtain summary statistics.
+    Summary statistics are obtained using two methods: direct calculation assuming skew-normal and 
+    fitting using scipy's curve_fit assuming Gaussian.
+    """
+    logger.info(f"Calculating the logdistance PDF for galaxies in {survey} | FP: {list(FPparams)}")
+
+    # Get some redshift-distance lookup tables
+    red_spline, lumred_spline, dist_spline, lumdist_spline, ez_spline = rz_table()
+    dz = sp.interpolate.splev(df["z_cmb"].to_numpy() / LIGHTSPEED, dist_spline)
+    dz_cluster = sp.interpolate.splev(df["z_dist_est"], dist_spline)
+    logger.info(f'Number of {survey} data = {len(df)}.')
+
+    # Define the range of logdists values to be calculated
+    dmin, dmax, nd = -1.5, 1.5, 1001
+    dbins = np.linspace(dmin, dmax, nd, endpoint=True)
+
+    pv_model = TwoMPP_SDSS_6dF(verbose=True) # type: ignore
+
+    if with_pv_model:
+        # Calculate predicted PVs using observed group redshift in CMB frame, and calculate cosmological redshift
+        df['v_pec'] = pv_model.calculate_pv(df['ra'].to_numpy(), df['dec'].to_numpy(), df['z_dist_est'].to_numpy())
+        df['z_pec'] = df['v_pec'] / LIGHTSPEED
+        df['z_cosmo'] = ((1 + df['z_dist_est']) / (1 + df['z_pec'])) - 1
+        d_H_pred = sp.interpolate.splev(df['z_cosmo'].to_numpy(), dist_spline, der=0)
+        
+        # Calculate predicted logdistance-ratios (subtract with prediction from PV model)
+        d_z = sp.interpolate.splev(df['z_dist_est'].to_numpy(), dist_spline, der=0)
+        df['logdist_pred'] = np.log10(d_z / d_H_pred)
+        df['r_true'] = df['r'] - df['logdist_pred']
+
+        # Add suffix to FPmethod
+        FPmethod += '_pv_model'
+    else:
+        df['r_true'] = df['r']
+
+    # Calculate full FN
+    d_H = np.outer(10.0**(-dbins), dz_cluster)
+    lmin = (SOLAR_MAGNITUDE['j'] + 5.0 * np.log10(1.0 + df["zhelio"].to_numpy()) + df["kcor_j"].to_numpy() + df["extinction_j"].to_numpy() + 10.0 - 2.5 * np.log10(2.0 * math.pi) + 5.0 * np.log10(d_H) - mag_high) / 5.0
+    lmax = (SOLAR_MAGNITUDE['j'] + 5.0 * np.log10(1.0 + df["zhelio"].to_numpy()) + df["kcor_j"].to_numpy() + df["extinction_j"].to_numpy() + 10.0 - 2.5 * np.log10(2.0 * math.pi) + 5.0 * np.log10(d_H) - mag_low) / 5.0
+    
+    # Calculate negative of log likelihood
+    loglike = FP_func(FPparams, dbins, df["z_cmb"].to_numpy(), df["r_true"].to_numpy(), df["s"].to_numpy(), df["i"].to_numpy(), df["er"].to_numpy(), df["es"].to_numpy(), df["ei"].to_numpy(), np.ones(len(df)), smin, df["lmin"].to_numpy(), df["lmax"].to_numpy(), df["C_m"].to_numpy(), sumgals=False, use_full_fn=use_full_fn)
+    
+    # Calculate full FN
+    FNvals = FN_func(FPparams, df["z_cmb"].to_numpy(), df["er"].to_numpy(), df["es"].to_numpy(), df["ei"].to_numpy(), lmin, lmax, smin)
+
+    # Calculate log prior
+    log_prior = -3 * dbins * np.log(10)
+    log_prior_tiles = np.tile(log_prior, (FNvals.shape[1], 1)).T
+
+    # Convert to the PDF for logdistance
+    logP_dist = - loglike - FNvals #+ log_prior_tiles
+
+    # normalise logP_dist (trapezoidal rule)
+    ddiff = np.log10(d_H[:-1]) - np.log10(d_H[1:])
+    valdiff = np.exp(logP_dist[1:]) + np.exp(logP_dist[0:-1])
+    norm_ = 0.5 * np.sum(valdiff * ddiff, axis=0)
+
+    logP_dist -= np.log(norm_[:, None]).T
+
+    # Calculate the mean and standard deviation of the distribution
+    mean = np.sum(dbins[0:-1,None]*np.exp(logP_dist[0:-1])+dbins[1:,None]*np.exp(logP_dist[1:]), axis=0)*(dbins[1]-dbins[0])/2.0
+    err = np.sqrt(np.sum(dbins[0:-1,None]**2*np.exp(logP_dist[0:-1])+dbins[1:,None]**2*np.exp(logP_dist[1:]), axis=0)*(dbins[1]-dbins[0])/2.0 - mean**2)
+
+    # Calculate the skewness and cap it
+    gamma1 = (np.sum(dbins[0:-1,None]**3*np.exp(logP_dist[0:-1])+dbins[1:,None]**3*np.exp(logP_dist[1:]), axis=0)*(dbins[1]-dbins[0])/2.0 - 3.0*mean*err**2 - mean**3)/err**3
+    gamma1 = np.where(gamma1 > 0.99, 0.99, gamma1)
+    gamma1 = np.where(gamma1 < -0.99, -0.99, gamma1)
+
+    # Calculate the parameters of skew-normal distribution
+    delta = np.sign(gamma1)*np.sqrt(np.pi/2.0*1.0/(1.0 + ((4.0 - np.pi)/(2.0*np.abs(gamma1)))**(2.0/3.0)))
+    scale = err * np.sqrt(1.0 / (1.0 - 2.0 * delta**2 / np.pi))
+    loc = mean - scale * delta * np.sqrt(2.0 / np.pi)
+    alpha = delta / (np.sqrt(1.0 - delta**2))
+
+    # Store the skew-normal values calculated analytically to the dataframe
+    df[f"logdist_mean_{FPmethod.lower()}"] = mean
+    df[f"logdist_std_{FPmethod.lower()}"] = err
+    df[f"logdist_alpha_{FPmethod.lower()}"] = alpha
+    df[f"logdist_loc_{FPmethod.lower()}"] = loc
+    df[f"logdist_scale_{FPmethod.lower()}"] = scale
+
+    # Transpose the PDF and return to linear unit
+    y = np.exp(logP_dist.T)
+    # Save the posterior distributions
+    if save_posterior:
+        logdist_posterior_filepath = LOGDIST_POSTERIOR_OUTPUT_FILEPATH[survey]
+        np.save(logdist_posterior_filepath, y)
+
+    # Find mean and standard deviation of the distribution using curve_fit
+    logdist_mean = []
+    logdist_std = []
+    chisq = []
+    rmse = []
+    for i, y_ in enumerate(y):
+        popt, pcov = curve_fit(gaus, dbins, y_, p0=[mean[i], err[i]])
+        popt[1] = np.absolute(popt[1])
+        logdist_mean.append(popt[0])
+        logdist_std.append(popt[1])
+
+        # Calculate the chi-squared statistic for the fit
+        ypred = norm.pdf(dbins, popt[0], popt[1])
+        chisquare = np.sum((y_ - ypred)**2 / ypred, axis=0)
+        chisq.append(chisquare)
+
+        # Calculate RMSE statistic for the fit
+        ypred = norm.pdf(dbins, popt[0], popt[1])
+        rmse_ = np.sqrt((1 / dbins.shape[0]) * np.sum((y_ - ypred)**2, axis=0))
+        rmse.append(rmse_)
+
+    df[f'logdist_{FPmethod.lower()}'] = logdist_mean
+    df[f'logdist_err_{FPmethod.lower()}'] = logdist_std
+
+    # Calculate observational error (subtract from intrinsic error)
+    a, b, sigma1 = FPparams[0], FPparams[1], FPparams[5]
+
+    # Calculate logdist observational error from FP spectro and photo errors
+    e_XFP = np.sqrt(df['er']**2 + (b * df['ei'])**2 + 2 * (-1) * np.absolute(b * df['er'] * df['ei']))
+    err_photo = e_XFP
+    err_spectro = df['es']
+    sigma_r_obs = np.sqrt((a * err_spectro)**2 + err_photo**2)
+    df[f"logdist_obs_err_{FPmethod.lower()}"] = sigma_r_obs
+
+    # Calculate logdist fit's goodness-of-fit
+    df[f'logdist_fit_chisq_{FPmethod.lower()}'] = chisq
+    df[f'logdist_fit_rmse_{FPmethod.lower()}'] = rmse
+    
+    return df
+
+
+def compare_cf_vs_formula() -> None:
+    """
+    This function compares the mean and std obtained using analytical formula and the one obtained from scipy's curve_fit.
+    """
+    for survey in NEW_SURVEY_LIST:
+        df = pd.read_csv(LOGDIST_OUTPUT_FILEPATH[survey])
+        
+        # Compare Cullan's mean and curve_fit mean
+        fig, (ax1, ax2) = plt.subplots(ncols=2, figsize=(16, 6))
+        ax1.scatter(df['logdist_mean'], df['logdist_mean_cf'], s=1)
+        ax1.set_ylabel('Mean from curve_fit', size=14)
+        ax1.set_xlabel('Mean from formula', size=14)
+
+        # Compare Cullan's std and curve_fit std
+        ax2.scatter(df['logdist_std'], df['logdist_std_cf'], s=1)
+        ax2.set_ylabel('Standard deviation from curve_fit', size=14)
+        ax2.set_xlabel('Standard deviation from formula', size=14)
+
+        plt.savefig(CURVEFIT_COMPARISON_IMG_FILEPATH[survey], dpi=300)
+
+
+def plot_best_worst_posterior() -> None:
+    """
+    This function compares the least and most skewed posterior distributions.
+    """
+    for survey in NEW_SURVEY_LIST:
+        # Load the posterior distributions
+        df = pd.read_csv(LOGDIST_OUTPUT_FILEPATH[survey])
+        dbins = np.linspace(-1.5, 1.5, 1001, endpoint=True)
+        y = np.load(LOGDIST_POSTERIOR_OUTPUT_FILEPATH[survey])
+
+        # Grab the index with the best and worst chi-square (relative to a Gaussian distribution)
+        index_worst = df['logdist_chisq_cf'].sort_values().index[-1]
+        index_best = df['logdist_chisq_cf'].sort_values().index[0]
+
+        fig, (ax1, ax2) = plt.subplots(ncols=2, figsize=(16, 6))
+
+        # Plot the best posteriors
+        ax1.plot(dbins, y[index_best, :], label='Best posterior', color='k')
+        ax1.plot(dbins, norm.pdf(dbins, loc=df.loc[index_best, 'logdist_mean_cf'], scale=df.loc[index_best, 'logdist_std_cf']), label='curve fit')
+        ax1.plot(dbins, skewnormal(dbins, loc=df.loc[index_best, 'logdist_loc'], err=df.loc[index_best, 'logdist_scale'], alpha=df.loc[index_best, 'logdist_alpha']), c='red', ls='--', label='Skew-normal')
+        ax1.set_xlim(-1.0, 1.0)
+        ax1.set_xlabel(r'$\eta$', size=20)
+        ax1.set_ylabel(r'PDF', size=20)
+        ax1.legend(fontsize=14)
+
+        # Plot the worst posterior
+        ax2.plot(dbins, y[index_worst, :], label='Worst posterior', color='k')
+        ax2.plot(dbins, norm.pdf(dbins, loc=df.loc[index_worst, 'logdist_mean_cf'], scale=df.loc[index_worst, 'logdist_std_cf']), label='curve fit')
+        ax2.plot(dbins, skewnormal(dbins, loc=df.loc[index_worst, 'logdist_loc'], err=df.loc[index_worst, 'logdist_scale'], alpha=df.loc[index_worst, 'logdist_alpha']), c='red', ls='--', label='Skew-normal')
+        ax2.set_xlim(-1.0, 1.0)
+        ax2.set_xlabel(r'$\eta$', size=20)
+        ax2.set_ylabel(r'PDF', size=20)
+        ax2.legend(fontsize=14)
+
+        plt.savefig(POSTERIOR_SKEWNESS_IMG_FILEPATH[survey], dpi=300)
+
+
+def main():
+    try:
+        logger.info(f"{'=' * 50}")
+        logger.info('Fitting log-distance ratios...')
+        logger.info(f'Environment variable: SMIN_SETTING = {SMIN_SETTING}.')
+        
+        # FP base parameters
+        fp_base_params = ["a", "b", "rmean", "smean", "imean", "s1", "s2", "s3"]
+
+        # Load FP parameters for fitting
+        fp_methods_dict = {
+            "individual": {},
+            "combined": {},
+            "common_abc": {}
+        }
+        # 1. Individual and combined FP
+        fp_params = pd.read_csv(FP_FIT_FILEPATH, index_col=0)
+        for survey in SURVEY_LIST:
+            fp_methods_dict["individual"][survey] = fp_params.loc[survey][fp_base_params].to_numpy()
+            fp_methods_dict["combined"][survey] = fp_params.loc["ALL_COMBINED"][fp_base_params].to_numpy()
+
+        # 2. abc-fixed FP
+        fp_params = pd.read_csv(FP_FIT_ABC_FIXED_FILEPATH, index_col=0)
+        for survey in SURVEY_LIST:
+            fp_methods_dict["common_abc"][survey] = fp_params.loc[survey][fp_base_params].to_numpy()
+
+        # Iterate for all sample
+        for survey in SURVEY_LIST:
+            logger.info(f"Calculating log-distance ratio for {survey}")
+
+            # Survey's veldisp limit
+            smin = SURVEY_VELDISP_LIMIT[SMIN_SETTING][survey]
+
+            # Get input filename (outlier-rejected sample)
+            input_filepath = OUTLIER_REJECT_FP_SAMPLE_FILEPATHS[survey]
+            df = pd.read_csv(input_filepath)
+
+            # Derive logdists for all FP methods
+            for fp_method in fp_methods_dict:
+                params = fp_methods_dict[fp_method][survey]
+
+                for with_pv_model in [False, True]:
+                    df = fit_logdist(
+                        survey=survey,
+                        df=df,
+                        smin=smin,
+                        FPmethod=fp_method,
+                        FPparams=params,
+                        use_full_fn=USE_FULL_FN,
+                        save_posterior=True,
+                        with_pv_model=with_pv_model
+                        )
+
+            # Save logdist measurements
+            logdist_output_filepath = LOGDIST_OUTPUT_FILEPATH[survey]
+            df.to_csv(logdist_output_filepath, index=False)
+
+        logger.info('Fitting log-distance ratios successful!')
+    except Exception as e:
+        logger.error(f'Fitting log-distance ratios failed. Reason: {e}.', exc_info=True)
+
+
+if __name__ == "__main__":
+    main()
